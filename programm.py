865cdeec3693406c162fd997ef954ebc4c4b3149
Alexander <alexanderbatyuta@yandex.ru>
2024-11-05 02:16:19 +0700

    исправление по pep8

--- programm.py
+++ programm.py
@@ -8,8 +8,10 @@ def many_elements(massive):
 
     return ans
 
+
 def uniqal_elements_first(massive_a, massive_b):
-    massive_a, massive_b = many_elements(massive_a),  many_elements(massive_b)
+    """Проверка на уникальность несколько одинаковых экземпляров"""
+    massive_a, massive_b = many_elements(massive_a), many_elements(massive_b)
     ans_a, ans_b = [], []
     for elements in massive_a:
         if elements not in massive_b:
@@ -21,6 +23,7 @@ def uniqal_elements_first(massive_a, massive_b):
 
 
 def element_a_in_b(massive_a, massive_b):
+    """Проверка на присутствие элементов A в массиве B"""
     ans = many_elements(massive_a)
     ans_b = []
 
@@ -30,7 +33,9 @@ def element_a_in_b(massive_a, massive_b):
 
     return ans_b
 
+
 def uniqal_elements_second(massive_a, massive_b):
+    """Проверка на уникальность экземпляров"""
     massive_a, massive_b = set(massive_a), set(massive_b)
     ans_a, ans_b = [], []
     for elements in massive_a:
@@ -42,9 +47,9 @@ def uniqal_elements_second(massive_a, massive_b):
     return [ans_a, ans_b]
 
 
-
 def check_int_array(arr):
     return all(isinstance(x, int) for x in arr)
+
 
 def main():
     """Меню"""
@@ -64,13 +69,27 @@ def main():
         match menu_variable:
             case "1":
                 try:
-                    masive_first = list(map(int, input("Введите целые числовые элементы массива A: ").split()))
+                    masive_first = list(
+                        map(
+                            int,
+                            input(
+                                "Введите целые числовые элементы массива A: "
+                            ).split(),
+                        )
+                    )
                     check_int_array(masive_first)
                 except ValueError:
                     print("Элементы должны быть целыми числами!!")
             case "2":
                 try:
-                    masive_second = list(map(int, input("Введите целые числовые элементы массива B: ").split()))
+                    masive_second = list(
+                        map(
+                            int,
+                            input(
+                                "Введите целые числовые элементы массива B: "
+                            ).split(),
+                        )
+                    )
                     check_int_array(masive_second)
                 except ValueError:
                     print("Элементы должны быть целыми числами!!")
@@ -81,13 +100,17 @@ def main():
                     f"В массиве B:{(uniqal_elements_first(masive_first, masive_second))[1]}"
                 )
             case "4":
-                print("повторяющиеся элементы массива А, "\
-                    f"которые есть в массиве B: {element_a_in_b(masive_first, masive_second)}")
+                print(
+                    "повторяющиеся элементы массива А, "
+                    f"которые есть в массиве B: {element_a_in_b(masive_first, masive_second)}"
+                )
             case "5":
-                print(f"Элементы,которые есть только в массиве А: " \
-                      f"{(uniqal_elements_second(masive_first, masive_second))[0]}\n"
-                      f"Элементы,которые есть только в массиве B: " \
-                      f"{(uniqal_elements_second(masive_first, masive_second))[1]}")
+                print(
+                    f"Элементы,которые есть только в массиве А: "
+                    f"{(uniqal_elements_second(masive_first, masive_second))[0]}\n"
+                    f"Элементы,которые есть только в массиве B: "
+                    f"{(uniqal_elements_second(masive_first, masive_second))[1]}"
+                )
             case "6":
                 break
 
